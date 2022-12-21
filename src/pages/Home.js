c8dcd26637927ecc2fe1bb10183be407cf3df4c3
Blenda Orellana <blen.or90@gmail.com>
2022-12-21 17:04:56 -0600

    changed size of photos in carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,59 +3,22 @@ import './style.css';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-// import {
-//   MDBCard,
-//   MDBCardBody,
-//   MDBCardText,
-//   MDBCardImage
-// } from 'mdb-react-ui-kit';
-
-// export default function App() {
-//   return (
-
-//     <MDBCard className='card'>
-//       <MDBCardImage src={process.env.PUBLIC_URL + '/assets/images/coquitolasheslogo2.png'} alt="img1" className="coquitolashes2"/>
-//       <MDBCardBody>
-//         <MDBCardText>
-//           Some quick example text to build on the card title and make up the bulk of the card's content.
-//         </MDBCardText>
-//       </MDBCardBody>
-//     </MDBCard>
-//     // <div className="container text-center justify-content-center col-6-md col-3-sm">
-//     //   <div className="card-body">
-//     //     <img src={process.env.PUBLIC_URL + '/assets/images/coquitolasheslogo2.png'} alt="img1" className="coquitolashes1"></img>    
-//     //   </div>
-//     //   <br></br>
-//     //   <div className="card-body">
-//     //     <img src={process.env.PUBLIC_URL + '/assets/images/coquitolasheslogo2.png'} alt="img1" className="coquitolashes1"></img>    
-//     //   </div>
-//     //   <br></br>
-//     //   <div className="card-body">
-//     //     <img src={process.env.PUBLIC_URL + '/assets/images/coquitolasheslogo2.png'} alt="img1" className="coquitolashes1"></img>    
-//     //   </div>
-
-//     // </div>
-//   );
-// }
-
-
-// 
 
 export default function App() {
   return (
 
     <Carousel>
-      <div>
+      <div className='container col-6'>
         <img src={process.env.PUBLIC_URL + '/assets/images/coquitologo1.jpg'} className='coquito'/>
-        <p className="coquitologo1">Classic Lashes</p>
+        <p className="service">Classic Lashes</p>
       </div>
-      <div>
+      <div className='container col-6'>
       <img src={process.env.PUBLIC_URL + '/assets/images/coquitologo1.jpg'} className='coquito'/>
-        <p className="coquitologo1">Classic Lashes</p>
+        <p className="service">Classic Lashes</p>
       </div>
-      <div>
+      <div className='container col-6'>
       <img src={process.env.PUBLIC_URL + '/assets/images/coquitologo1.jpg'} className='coquito'/>
-        <p className="coquitologo1">Classic Lashes</p>
+        <p className="service">Classic Lashes</p>
       </div>
     </Carousel>
   );
